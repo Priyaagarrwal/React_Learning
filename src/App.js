ec503344002057e1994e8d2792ec10442ac58643
Priya Agarwal <priyaagarwal1115@gmail.com>
2023-03-16 23:19:33 +1200

    props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import {Header} from './components/Header'
 import CHeader from './components/CHeader'
 class App extends React.Component
 {
   render(){
     return (
       <div className="App">
-      <Header/>
+      <Header name="Anuj" last="sharma">
+      <p> Anuj is a Good Boy</p>
+      </Header>
+      <Header name="vivek" last="verma"/>
+      <Header name="rahul"/>
+
       <CHeader/>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React from 'react';
-function Header(){
-return <h1>Hello Functional Component</h1>
+export const Header=(props) =>{
+    console.log(props);
+return(
+    <div>
+<h1>Hello {props.name} - {props.last}</h1>
+{props.children}
+</div>
+)
 }
-export default Header;
